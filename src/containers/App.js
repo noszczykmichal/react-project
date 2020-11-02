6d7f6ac5ca2b609b8f6392d353534ec5b44bb9e6
Michał Noszczyk <noszczykmichal@wp.pl>
2020-11-02 22:03:44 +0100

    added hoc-WithClass in app.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../hoc/WithClass';
 
 class App extends Component {
   constructor(props){
@@ -84,7 +85,7 @@ class App extends Component {
 
     return (
 
-      <div className={classes.App}>
+      <WithClass classes={classes.App}>
         <button onClick={()=>{this.setState({showCockpit:false})}}>Remove Cockpit</button>
         {this.state.showCockpit ? (
           <Cockpit 
@@ -94,7 +95,7 @@ class App extends Component {
         clicked={this.togglePersonsHandler}/>
         ) : null}
         {persons}
-      </div>
+      </WithClass>
 
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
